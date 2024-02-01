e1c9c96fe4c53582e1371aab01c5bda49a4f3dc8
Skillter <57605954+Skillter@users.noreply.github.com>
2024-02-01 07:21:07 +0100

    Update Main.groovy

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,10 +1,13 @@
-import calculator.Calculator
+import filereader.NumberReader
 
 class HelloWorld {
 
 
   static void main(String[] args) {
-    println Calculator.division([100, 5, 0])
+    // println Calculator.division([100, 5, 2])
+
+    NumberReader.readResourceFiles()
+
   }
 
 
